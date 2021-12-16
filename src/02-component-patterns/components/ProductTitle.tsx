0ea3a49f83db66faa53b8fb561d43b928cc7acea
lmiret <16326209+lorenzoMrt@users.noreply.github.com>
2021-12-16 17:42:20 +0100

    [ADD] Styling of custom components

--- src/02-component-patterns/components/ProductTitle.tsx
+++ src/02-component-patterns/components/ProductTitle.tsx
@@ -2,7 +2,17 @@ import { useContext } from "react";
 import styles from '../styles/styles.module.css';
 import { ProductContext } from "./ProductCard";
 
-export const ProductTitle= ({title }: {title?: string}) => {
-    const {product} = useContext(ProductContext);
-    return(<span className={styles.productDescription}> {title ? title : product.title} </span>)
+export interface Props {
+  title?: string;
+  className?: string;
 }
+
+export const ProductTitle = ({ title, className }: Props) => {
+  const { product } = useContext(ProductContext);
+  return (
+    <span className={`${styles.productDescription} ${className}`}>
+      {" "}
+      {title ? title : product.title}{" "}
+    </span>
+  );
+};
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,35 +1,36 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components"
-
+import "../styles/custom-styles.css";
 
 const product = {
-    id: '1',
-    title: 'Another Coffee mug',
-    img: './coffee-mug.png'
-}
+  id: "1",
+  title: "Another Coffee mug",
+  img: "./coffee-mug.png",
+};
 
 export const ShoppingPage = () => {
-    return (
-        <div>
-            <h1>Shopping store</h1>
-            <hr/>
-            <div style= {{
-                display: 'flex',
-                flexDirection: 'row',
-                flexWrap: 'wrap'
-            }}>
-                
-                <ProductCard product={product}>
-                    <ProductImage/>
-                    <ProductTitle title='Depressive mug'/>
-                    <ProductButtons/>
-                </ProductCard>
-                
-                <ProductCard product={product}>
-                    <ProductCard.Image/>
-                    <ProductCard.Title/>
-                    <ProductCard.Buttons/>
-                </ProductCard>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Shopping store</h1>
+      <hr />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        <ProductCard product={product} className="bg-dark">
+          <ProductImage className="custom-image" />
+          <ProductTitle title="Depressive mug" className="text-white" />
+          <ProductButtons />
+        </ProductCard>
+
+        <ProductCard product={product}>
+          <ProductCard.Image />
+          <ProductCard.Title />
+          <ProductCard.Buttons />
+        </ProductCard>
+      </div>
+    </div>
+  );
+};
